844fbfd76b6f136f5535af2f983d1aee6050ede4
Tobias Mayer <tobi.mayer1990@gmail.com>
2023-03-31 17:29:37 +0200

    test data pipeline and model

--- train.py
+++ train.py
@@ -4,20 +4,31 @@ import torch
 
 from data import get_train_test_dataloader
 
+BATCH_SIZE = 1
+
 # device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 device = torch.device('cpu')
 
 model = Model().to(device)
 
-train_loader, test_loader = get_train_test_dataloader(8)
+train_loader, test_loader = get_train_test_dataloader(1)
 
 
 import matplotlib.pyplot as plt
 
-X, Y = next(iter(train_loader))
-print(f"Feature batch shape: {train_features.size()}")
-print(f"Labels batch shape: {train_labels.size()}")
-img = train_features[0].squeeze()
-label = train_labels[0]
-plt.imshow(img, cmap="gray")
+batch = next(iter(train_loader))
+X = batch['image']
+Y = batch['depth']
+print(f"X batch shape: {X.size()}")
+print(f"Y batch shape: {Y.size()}")
+
+# img = X[0]
+# label = Y[0]
+# plt.imshow(img.permute((1, 2, 0)), cmap="gray")
+# plt.show()
+
+output = model(X)
+print(output.shape)
+
+plt.imshow(output[0].detach().permute((1, 2, 0)))
 plt.show()
